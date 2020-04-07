5a0d7d61bdf7723876baaf335c1e6591731cf41e
Gustavo Rodrigues <kust44@github.com>
2020-04-07 16:10:42 -0300

    Roteamento para página do curso singularmente via ID

---
+++ public/solo.js
@@ -0,0 +1,22 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const modal = document.querySelector('.modal');
+const card = document.querySelector('.content.solo');
+
+card.addEventListener("click", function(){
+    const pageID = content.getAttribute("id");
+    modalOverlay.classList.add('active');
+    modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${pageID}`;
+});
+
+document.querySelector(".close-modal").addEventListener("click", function(){
+    modalOverlay.classList.remove("active");
+    modalOverlay.querySelector("iframe").src = "";
+})
+
+document.querySelector(".maximize-modal").addEventListener("click", function(){
+    if (modal.classList.contains("maximized")) {
+        modal.classList.remove("maximized");
+    } else {
+        modal.classList.add("maximized");
+    }
+})
--- server.js
+++ server.js
@@ -10,7 +10,8 @@ server.set("view engine", "njk");
 
 nunjucks.configure("views", {
     express: server,
-    autoescape: false
+    autoescape: false,
+    noCache: true
 })
 
 server.get('/', (req, res) => {
@@ -23,6 +24,25 @@ server.get('/sobre', (req, res) => {
 
 server.get('/conteudos', (req, res) => {
     return res.render("conteudos", { items: content });
+});
+
+server.get('/conteudos/:id', (req, res) => {
+    const id = req.params.id;
+    let page = {};
+    let flag = 0;
+
+    for (let i = 0; i < content.length; i++) {
+        if (id == content[i].id) {
+            page = content[i];
+            flag++;
+        }
+    }
+
+    if (flag == 0) {
+        return res.send("Page not found");
+    }
+
+    return res.render("page", { item: page });
 });
 
 server.use(function(req, res) {
